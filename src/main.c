a8fbd050cbc86c23b061379bca9e3d3eacd1eec6
Qball Cow <Qball@Sarine.nl>
2008-02-06 13:44:03 +0100

    If there is an instance allready running, make it create a new main-window.

--- src/main.c
+++ src/main.c
@@ -8,7 +8,31 @@
 
 GList *interface_list = NULL;
 
+/**
+ * Interprocess communication. Used to make sure
+ * only one instance is running
+ */
 BaconMessageConnection *bacon_connection = NULL;
+
+/**
+ * Handle ipc messages
+ */
+static void bacon_on_message_received(const char *message, gpointer data)
+{
+    StuffKeeperDataBackend *skdb = STUFFKEEPER_DATA_BACKEND(data);
+    printf("IPC: got '%s'\n", (message)?message:"(null)");
+    if(message)
+    {
+        if(strcmp(message, "New Window") == 0)
+        {
+            StuffKeeperInterface *ski= stuffkeeper_interface_new();
+            interface_list = g_list_append(interface_list, ski);
+            stuffkeeper_interface_initialize_interface(ski,skdb);
+            printf("IPC: Requested new window\n");
+        }
+    }
+}
+
 /**
  * The main programs
  */
@@ -31,28 +55,9 @@ int main ( int argc, char **argv )
         /* return a failure */
         return EXIT_FAILURE;
     }
-    
-    /* bacon here we come */
-    bacon_connection = bacon_message_connection_new("stuffkeeper");
-    if(bacon_connection) 
-    {
-        if (!bacon_message_connection_get_is_server (bacon_connection)) 
-        {
-            /* There is an instant allready running */
-
-            /* Returning */
-            printf("There is allready an instance running. quitting\n");
-            exit(EXIT_SUCCESS);
-        }
-        /* setup signal handler */
-
-    }
 
     /* Initialize the backend */
     skdb = stuffkeeper_data_backend_new();
-
-
-
     /* Check if creating the backend worked */
     if(!skdb)
     {
@@ -60,7 +65,43 @@ int main ( int argc, char **argv )
         return EXIT_FAILURE;
     }
 
- 
+    
+    /* Setup the interprocess communication. this does not work in win32! */ 
+    bacon_connection = bacon_message_connection_new("stuffkeeper");
+    if(bacon_connection) 
+    {
+        /* Check if we are the only instance, this can be checked by looking if we are the server */
+        if (!bacon_message_connection_get_is_server (bacon_connection)) 
+        {
+            /* There is an instant allready running */
+            /* message the running instance, to show a new window */
+            bacon_message_connection_send(bacon_connection, "New Window");
+
+            /* Close the connection */
+            bacon_message_connection_free (bacon_connection);
+            /* Returning */
+            printf("There is allready an instance running. quitting\n");
+            g_object_unref(skdb); 
+            exit(EXIT_SUCCESS);
+        }
+        /* setup signal handler */
+        /* Listen for incoming messages */
+        bacon_message_connection_set_callback (bacon_connection,
+                bacon_on_message_received,
+                    skdb);
+    }
+    else
+    {
+        /* print an error and quit */
+        printf("Failed to setup IPC, quitting\n");
+        g_object_unref(skdb); 
+        /* Return error code */
+        exit(EXIT_FAILURE);
+    }
+
+  
+
+    
     ski= stuffkeeper_interface_new();
     interface_list = g_list_append(interface_list, ski);
     stuffkeeper_interface_initialize_interface(ski,skdb);
