ebb458afd3c21c3784bef571069cac770d30312f
Qball Cow <Qball@Sarine.nl>
2008-03-24 12:59:00 +0100

    Fix csv export to correctly escape, and fix compile error

--- src/sexy-icon-entry.c
+++ src/sexy-icon-entry.c
@@ -18,7 +18,7 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA  02111-1307, USA.
  */
-#include <libsexy/sexy-icon-entry.h>
+#include "sexy-icon-entry.h"
 #include <string.h>
 #include <gtk/gtk.h>
 
