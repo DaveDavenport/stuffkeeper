b5e4a729c262c94d3a97bb2d867a2344cb1b1e5a
Qball Cow <qball@QDesk.site>
2008-07-20 00:56:01 +0200

    fix path

--- vala/build_plugin.sh
+++ vala/build_plugin.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-valac  --vapidir=../stuffkeeper/vala/ --pkg=gtk+-2.0 --pkg=glib-2.0 --pkg=gmodule-2.0 --pkg=stuffkeeper test.vala   --Xcc="-Wl,--export-dynamic" --Xcc="-Wl,-soname" --Xcc="-shared" --Xcc="-Wl,--module" --Xcc="-fPIC" --debug --save-temps
+valac  --vapidir=. --pkg=gtk+-2.0 --pkg=glib-2.0 --pkg=gmodule-2.0 --pkg=stuffkeeper test.vala   --Xcc="-Wl,--export-dynamic" --Xcc="-Wl,-soname" --Xcc="-shared" --Xcc="-Wl,--module" --Xcc="-fPIC" --debug 
