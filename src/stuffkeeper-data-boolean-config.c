b49e40ed34072ee6f4526a28c6b08839f188a905
Qball Cow <qball@s010375.(none)>
2008-07-25 08:05:52 +0200

    Fix double dispose on vala file

--- src/stuffkeeper-data-boolean-config.c
+++ src/stuffkeeper-data-boolean-config.c
@@ -1,5 +1,6 @@
 
 #include "stuffkeeper-data-boolean-config.h"
+#include <stdio.h>
 
 
 
@@ -14,6 +15,7 @@ struct _StuffkeeperDataBooleanConfigPrivate {
 enum  {
 	STUFFKEEPER_DATA_BOOLEAN_CONFIG_DUMMY_PROPERTY
 };
+static gboolean stuffkeeper_data_boolean_config_quit;
 static void _stuffkeeper_data_boolean_config_field_changed_stuffkeeper_data_schema_schema_custom_field_changed (StuffkeeperDataSchema* _sender, const char* id, gint field, gpointer self);
 static void stuffkeeper_data_boolean_config_field_changed (StuffkeeperDataBooleanConfig* self, StuffkeeperDataSchema* schema, const char* id, gint field);
 static void stuffkeeper_data_boolean_config_toggled (StuffkeeperDataBooleanConfig* self, GtkToggleButton* toggle);
@@ -74,21 +76,21 @@ static void _stuffkeeper_data_boolean_config_toggled_gtk_toggle_button_toggled (
  * Set it up 
  */
 void stuffkeeper_data_boolean_config_setup (StuffkeeperDataBooleanConfig* self, StuffkeeperDataSchema* schema, const char* fid) {
+	gint value;
 	StuffkeeperDataSchema* _tmp1;
 	StuffkeeperDataSchema* _tmp0;
 	char* _tmp3;
 	const char* _tmp2;
-	gint value;
 	g_return_if_fail (STUFFKEEPER_IS_DATA_BOOLEAN_CONFIG (self));
 	g_return_if_fail (STUFFKEEPER_IS_DATA_SCHEMA (schema));
 	g_return_if_fail (fid != NULL);
+	value = 0;
 	_tmp1 = NULL;
 	_tmp0 = NULL;
 	self->priv->schema = (_tmp1 = (_tmp0 = schema, (_tmp0 == NULL ? NULL : g_object_ref (_tmp0))), (self->priv->schema == NULL ? NULL : (self->priv->schema = (g_object_unref (self->priv->schema), NULL))), _tmp1);
 	_tmp3 = NULL;
 	_tmp2 = NULL;
 	self->priv->field = (_tmp3 = (_tmp2 = fid, (_tmp2 == NULL ? NULL : g_strdup (_tmp2))), (self->priv->field = (g_free (self->priv->field), NULL)), _tmp3);
-	value = 0;
 	if (stuffkeeper_data_schema_get_custom_field_integer (schema, self->priv->field, 0, &value)) {
 		gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (self->priv->checkbox), ((gboolean) (value)));
 	}
@@ -120,9 +122,9 @@ static GObject * stuffkeeper_data_boolean_config_constructor (GType type, guint 
 	{
 		GtkLabel* label;
 		GtkCheckButton* _tmp0;
-		label = g_object_ref_sink (((GtkLabel*) (gtk_label_new ("Default value"))));
+		label = g_object_ref_sink (gtk_label_new ("Default value"));
 		_tmp0 = NULL;
-		self->priv->checkbox = (_tmp0 = g_object_ref_sink (((GtkCheckButton*) (gtk_check_button_new ()))), (self->priv->checkbox == NULL ? NULL : (self->priv->checkbox = (g_object_unref (self->priv->checkbox), NULL))), _tmp0);
+		self->priv->checkbox = (_tmp0 = g_object_ref_sink (gtk_check_button_new ()), (self->priv->checkbox == NULL ? NULL : (self->priv->checkbox = (g_object_unref (self->priv->checkbox), NULL))), _tmp0);
 		gtk_box_pack_start (GTK_BOX (self), GTK_WIDGET (label), FALSE, FALSE, ((guint) (0)));
 		gtk_box_pack_start (GTK_BOX (self), GTK_WIDGET (self->priv->checkbox), TRUE, TRUE, ((guint) (0)));
 		gtk_widget_show_all (GTK_WIDGET (self));
@@ -137,6 +139,7 @@ static void stuffkeeper_data_boolean_config_class_init (StuffkeeperDataBooleanCo
 	g_type_class_add_private (klass, sizeof (StuffkeeperDataBooleanConfigPrivate));
 	G_OBJECT_CLASS (klass)->constructor = stuffkeeper_data_boolean_config_constructor;
 	G_OBJECT_CLASS (klass)->dispose = stuffkeeper_data_boolean_config_dispose;
+	stuffkeeper_data_boolean_config_quit = ((gboolean) (0));
 }
 
 
@@ -152,8 +155,13 @@ static void stuffkeeper_data_boolean_config_dispose (GObject * obj) {
 	StuffkeeperDataBooleanConfig * self;
 	self = STUFFKEEPER_DATA_BOOLEAN_CONFIG (obj);
 	{
-		guint _tmp1;
-		g_signal_handlers_disconnect_matched (self->priv->schema, G_SIGNAL_MATCH_ID | G_SIGNAL_MATCH_FUNC | G_SIGNAL_MATCH_DATA, (g_signal_parse_name ("schema-custom-field-changed", STUFFKEEPER_TYPE_DATA_SCHEMA, &_tmp1, NULL, FALSE), _tmp1), 0, NULL, ((GCallback) (_stuffkeeper_data_boolean_config_field_changed_stuffkeeper_data_schema_schema_custom_field_changed)), self);
+		fprintf (stdout, "Dispose\n");
+		/* This gets called twice.. */
+		if (!stuffkeeper_data_boolean_config_quit) {
+			guint _tmp1;
+			g_signal_handlers_disconnect_matched (self->priv->schema, G_SIGNAL_MATCH_ID | G_SIGNAL_MATCH_FUNC | G_SIGNAL_MATCH_DATA, (g_signal_parse_name ("schema-custom-field-changed", STUFFKEEPER_TYPE_DATA_SCHEMA, &_tmp1, NULL, FALSE), _tmp1), 0, NULL, ((GCallback) (_stuffkeeper_data_boolean_config_field_changed_stuffkeeper_data_schema_schema_custom_field_changed)), self);
+		}
+		stuffkeeper_data_boolean_config_quit = TRUE;
 	}
 	self->priv->field = (g_free (self->priv->field), NULL);
 	(self->priv->schema == NULL ? NULL : (self->priv->schema = (g_object_unref (self->priv->schema), NULL)));
@@ -163,12 +171,10 @@ static void stuffkeeper_data_boolean_config_dispose (GObject * obj) {
 
 
 GType stuffkeeper_data_boolean_config_get_type (void) {
-	static volatile gsize stuffkeeper_data_boolean_config_type_id = 0;
-	if (g_once_init_enter (&stuffkeeper_data_boolean_config_type_id)) {
-		GType stuffkeeper_data_boolean_config_type_id_temp;
+	static GType stuffkeeper_data_boolean_config_type_id = 0;
+	if (G_UNLIKELY (stuffkeeper_data_boolean_config_type_id == 0)) {
 		static const GTypeInfo g_define_type_info = { sizeof (StuffkeeperDataBooleanConfigClass), (GBaseInitFunc) NULL, (GBaseFinalizeFunc) NULL, (GClassInitFunc) stuffkeeper_data_boolean_config_class_init, (GClassFinalizeFunc) NULL, NULL, sizeof (StuffkeeperDataBooleanConfig), 0, (GInstanceInitFunc) stuffkeeper_data_boolean_config_instance_init };
-		stuffkeeper_data_boolean_config_type_id_temp = g_type_register_static (GTK_TYPE_HBOX, "StuffkeeperDataBooleanConfig", &g_define_type_info, 0);
-		g_once_init_leave (&stuffkeeper_data_boolean_config_type_id, stuffkeeper_data_boolean_config_type_id_temp);
+		stuffkeeper_data_boolean_config_type_id = g_type_register_static (GTK_TYPE_HBOX, "StuffkeeperDataBooleanConfig", &g_define_type_info, 0);
 	}
 	return stuffkeeper_data_boolean_config_type_id;
 }
--- src/stuffkeeper-data-boolean-config.vala
+++ src/stuffkeeper-data-boolean-config.vala
@@ -3,76 +3,86 @@ using Gtk;
 using Stuffkeeper;
 
 public class Stuffkeeper.DataBooleanConfig : Gtk.HBox {
-		private string field = null;
-		private DataSchema schema = null;
-		private Gtk.CheckButton checkbox = null;
+	private string field = null;
+	private DataSchema schema = null;
+	private Gtk.CheckButton checkbox = null;
+	/* hack to fix vala bug */
+	static bool quit = 0;
 
-		/**
-		 * Listen to changes to this field
-		 */
-		private void field_changed(DataSchema schema, string id, int field)
+	/**
+	 * Listen to changes to this field
+	 */
+	private void field_changed(DataSchema schema, string id, int field)
+	{
+		if(field == 0)
 		{
-			if(field == 0)
+			int value = 0;
+			if(schema.get_custom_field_integer(id, 0, out value))
 			{
-				int value = 0;
-				if(schema.get_custom_field_integer(id, 0, out value))
+				if(checkbox.active != (bool)value)
 				{
-					if(checkbox.active != (bool)value)
-					{
-						schema.schema_custom_field_changed -= field_changed;
-						checkbox.active = (bool)value;	
-						schema.schema_custom_field_changed += field_changed;
-					}
+					schema.schema_custom_field_changed -= field_changed;
+					checkbox.active = (bool)value;	
+					schema.schema_custom_field_changed += field_changed;
 				}
-			}	
-		}
+			}
+		}	
+	}
 
-		/**
-		 * Listen to clicks
-		 */	
-		private void toggled(Gtk.ToggleButton toggle)
+	/**
+	 * Listen to clicks
+	 */	
+	private void toggled(Gtk.ToggleButton toggle)
+	{
+		if(toggle.active)
 		{
-			if(toggle.active)
-			{
-				schema.set_custom_field_integer(field,0,1);
-			}else{
-				schema.set_custom_field_integer(field,0,0);
-			}
+			schema.set_custom_field_integer(field,0,1);
+		}else{
+			schema.set_custom_field_integer(field,0,0);
 		}
+	}
 
-		/**
-		 * Destruction 
-		 */
-		~DataBooleanConfig() {
-				schema.schema_custom_field_changed -= field_changed;
-		}
-		/**
-		 * Construct 
-		 */
-		construct {
-			var label = new Gtk.Label("Default value");
-			this.checkbox = new Gtk.CheckButton();
-			
-			this.pack_start(label, false, false,0);
-			this.pack_start(checkbox, true, true,0);
-			this.show_all();
-		}
+	/**
+	 * Destruction 
+	 */
+	~DataBooleanConfig() {
+		stdout.printf("Dispose\n");
 
-		/**
-		 * Set it up 
-		 */
-		public void setup (Stuffkeeper.DataSchema schema, string fid) {
-			int value;
-			this.schema = schema;
-			this.field = fid;
-
-			if(schema.get_custom_field_integer(field, 0, out value))
-			{
-					this.checkbox.active = (bool)value;
-			}
-			/* connect signals */
-			this.checkbox.toggled += toggled;	
-			schema.schema_custom_field_changed += field_changed;
+		/* This gets called twice.. 
+		 * Hack it to have it run only once*/
+		if(!quit)
+		{
+			schema.schema_custom_field_changed -= field_changed;
 		}
+		quit = true;
+	}
+	/**
+	 * Construct 
+	 */
+	construct {
+		var label = new Gtk.Label("Default value");
+		this.checkbox = new Gtk.CheckButton();
+
+		this.pack_start(label, false, false,0);
+		this.pack_start(checkbox, true, true,0);
+		this.show_all();
+	}
+
+	/**
+	 * Set it up 
+	 */
+	public void setup (Stuffkeeper.DataSchema schema, string fid) {
+		int value;
+		this.schema = schema;
+		this.field = fid;
+
+		if(schema.get_custom_field_integer(field, 0, out value))
+		{
+			this.checkbox.active = (bool)value;
+		}
+		/* connect signals */
+		this.checkbox.toggled += toggled;	
+		schema.schema_custom_field_changed += field_changed;
+	}
 
 }
